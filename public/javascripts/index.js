4d9a17d4f552dbd35112eb99d426b6f417486291
xyq565861 <xyq565861@outlook.com>
2015-11-13 14:26:05 +0900

    可视化完成
    可视化完成

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -17,8 +17,33 @@ for(var i = 0; i< lis.length; i++){
 var xhr = new XMLHttpRequest();
 var ac = new window.AudioContext();
 var gainNode = ac.createGain();
+var analyser = ac.createAnalyser();
+var size = 64;
+analyser.fftSize = size*4;
+analyser.connect(gainNode);
 var source = null;
 var n = 0;
+var box = $("#box")[0];
+var height,width;
+var canvas = document.createElement("canvas");
+box.appendChild(canvas);
+var ctx = canvas.getContext("2d");
+resize();
+window.onresize = resize;
+
+function resize(){
+    height = box.clientHeight;
+    width = box.clientWidth;
+    canvas.height = height;
+    canvas.width = width;
+    var line = ctx.createLinearGradient(0,0,0,height);
+    line.addColorStop(0,"red");
+    line.addColorStop(0.5,"green");
+    line.addColorStop(1,"blue");
+    ctx.fillStyle = line;
+    
+}
+
 gainNode.connect(ac.destination);
 $("#difficult")[0].onmousemove = function(){
 	changeVolume(this.value/this.max);
@@ -29,10 +54,29 @@ function changeVolume(soundvalue){
 
 }
 
+function visualizer(){
+    var arr = new Uint8Array(analyser.frequencyBinCount)
+    requestAnimationFrame = window.requestAnimationFrame;
+    function v(){
+        analyser.getByteFrequencyData(arr);
+        draw(arr);
+        requestAnimationFrame(v);
+    }    
+}
+
+function draw(arr){
+    ctx.clearRect(0,0,width,height);
+    var w = width/size;
+    for (var i = 0;i<size;i++){
+        var h = arr[i]/256 *height;
+        ctx.fillRect(w * i, height - h, w*0.8);
+        
+    }
+}
 function start(buffer){    
     var bufferSource = ac.createBufferSource();
     bufferSource.buffer = buffer;
-    bufferSource.connect(gainNode);
+    bufferSource.connect(analyser);
     if(n > 1){
     n--;
     return;
